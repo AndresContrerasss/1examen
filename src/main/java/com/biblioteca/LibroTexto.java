14d4f83de98d8dc38dbb6e3d7b7dec0a22c46af9
Germi <gdbolanos@estudiante.uniajc.com.co>
2026-09-22 23:58:35 -0500

    crear librotexto

---
+++ src/main/java/com/biblioteca/LibroTexto.java
@@ -0,0 +1,8 @@
+package com.biblioteca;
+
+/**
+ * LibroTexto hereda de Libro y agrega el atributo curso,
+   que indica a qué curso está asociado el libro de texto.
+ */
+public class LibroTexto extends Libro {   
+}

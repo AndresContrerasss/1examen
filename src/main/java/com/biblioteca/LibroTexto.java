eec86959e1fabb9c413ddd4a614cb1e032e40b54
Germi <gdbolanos@estudiante.uniajc.com.co>
2026-09-23 00:20:51 -0500

    creacion de metodo librotexto

--- src/main/java/com/biblioteca/LibroTexto.java
+++ src/main/java/com/biblioteca/LibroTexto.java
@@ -2,7 +2,37 @@ package com.biblioteca;
 
 /**
  * LibroTexto hereda de Libro y agrega el atributo curso,
-   que indica a qué curso está asociado el libro de texto.
+ * que indica a qué curso está asociado el libro de texto.
  */
-public class LibroTexto extends Libro {   
+public class LibroTexto extends Libro {
+
+    private String curso;
+
+    public LibroTexto() {
+        super(); // llama al constructor vacío de Libro
+        this.curso = "";
+    }
+
+    public LibroTexto(String titulo, String autor, int numeroEjemplares,
+                       int numeroEjemplaresPrestados, String curso) {
+        super(titulo, autor, numeroEjemplares, numeroEjemplaresPrestados);
+        this.curso = curso;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    @Override
+    public String toString() {
+        return "LibroTexto [titulo=" + getTitulo()
+                + ", autor=" + getAutor()
+                + ", numeroEjemplares=" + getNumeroEjemplares()
+                + ", numeroEjemplaresPrestados=" + getNumeroEjemplaresPrestados()
+                + ", curso=" + curso + "]";
+    }
 }
